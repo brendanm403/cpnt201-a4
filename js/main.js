94b2740134d32423a9868da78ff26d0b073c444e
Brendan Malycky <bmalycky@hotmail.com>
2023-11-04 20:51:11 -0600

    displayed date information on page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ import '../css/style.css'
 import javascriptLogo from '../images/javascript.svg'
 import viteLogo from '../images/vite.svg'
 import { setupCounter } from './counter.js'
+import dayjs from "dayjs";
+
+const startDate = dayjs("2023-09-05");
+const currentDate = dayjs();
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -19,6 +23,8 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
+  <p>Todays Date: ${dayjs().format("MMMM D, YYYY")}</p>
+  <p>Days Since Start: ${currentDate.diff(startDate, "day")}</p>
 `
 
 setupCounter(document.querySelector('#counter'))
